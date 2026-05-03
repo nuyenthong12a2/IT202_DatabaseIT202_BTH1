7b70873520fc0805d683f689806a27ebcab62bd8
Nguyen THong <nguyenthong12122015@gmail.com>
2026-05-03 19:33:21 +0700

    03/05/2026 19:33 PM

--- BTH1.sql
+++ BTH1.sql
@@ -81,5 +81,21 @@ WHERE phone = '0987654321';
 
 DELETE FROM movies 
 WHERE id = 3;
+-- Liệt kê các bộ phim có thời lượng từ 90 đến 120 phút
+-- Sửa 'moives' thành 'movies'
+SELECT * FROM movies 
+WHERE duration_minutes BETWEEN 90 AND 120;
  
+SELECT * 
+FROM bookings 
+WHERE showtime_id = 2 
+ORDER BY booking_date DESC;
+SELECT * 
+FROM movies 
+WHERE age_restriction = 18 OR duration_minutes > 150;
+SELECT * 
+FROM showtimes 
+WHERE ticket_price > 100000 
+  AND MONTH(show_time) = MONTH(CURDATE()) 
+  AND YEAR(show_time) = YEAR(CURDATE());
 
